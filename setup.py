b30346fc79be6afc0257f31bc92fb43c2b6ecd69
Marco del Carmen <marco@mdelcarmen.me>
2022-11-16 13:30:54 -0600

    Temp fix deployment issues

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='method-python',
-    version='0.0.24',
+    version='0.0.25',
     description='Python library for the Method API',
     author='Marco del Carmen',
     author_email='marco@mdelcarmen.me',
