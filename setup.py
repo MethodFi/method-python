4e23be8ba8d3d1dd6d531531a2a07f09383292bc
Michael Ossig <mrossig201@gmail.com>
2024-04-05 11:50:07 -0400

    removes duplicate method (#34)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='method-python',
-    version='0.0.43',
+    version='0.0.44',
     description='Python library for the Method API',
     long_description='Python library for the Method API',
     long_description_content_type='text/x-rst',
