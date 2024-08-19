dafc453c2c8f2827461c3fc053cba271d38a9f37
Bilal Hussain <bilal@methodfi.com>
2024-08-19 10:09:44 -0500

    version fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='method-python',
-    version='1.1.2',
+    version='1.1.0',
     description='Python library for the Method API',
     long_description='Python library for the Method API',
     long_description_content_type='text/x-rst',
