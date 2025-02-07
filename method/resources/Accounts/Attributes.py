24769aded894641dc639cb7c2d25282b25d20685
Bilal Hussain <bilal@methodfi.com>
2025-02-07 11:36:13 -0600

    account products + attributes

---
+++ method/resources/Accounts/Attributes.py
@@ -0,0 +1,48 @@
+from typing import TypedDict, Optional, Literal, List, Any
+
+from method.resource import MethodResponse, Resource, ResourceListOpts
+from method.configuration import Configuration
+from method.errors import ResourceError
+
+
+class AccountAttribute(TypedDict):
+    value: Any
+
+
+class AccountAttributesType(TypedDict):
+    usage_pattern: AccountAttribute
+    account_standing: AccountAttribute
+    delinquent_period: AccountAttribute
+    delinquent_outcome: AccountAttribute
+    delinquent_amount: AccountAttribute
+    utilization: AccountAttribute
+
+AccountAttributesStatusesLiterals = Literal[
+    'completed',
+    'in_progress',
+    'pending',
+    'failed'
+]
+
+class AccountAttributes(TypedDict):
+    id: str
+    account_id: str
+    status: AccountAttributesStatusesLiterals
+    attributes: Optional[List[AccountAttributesType]]
+    error: Optional[ResourceError]
+    created_at: str
+    updated_at: str
+
+
+class AccountAttributesResource(Resource):
+    def __init__(self, config: Configuration):
+        super(AccountAttributesResource, self).__init__(config.add_path('attributes'))
+
+    def retrieve(self, acc_attr_id: str) -> MethodResponse[AccountAttributes]:
+        return super(AccountAttributesResource, self)._get_with_id(acc_attr_id)
+    
+    def list(self, params: Optional[ResourceListOpts] = None) -> MethodResponse[List[AccountAttributes]]:
+        return super(AccountAttributesResource, self)._list(params)
+
+    def create(self) -> MethodResponse[AccountAttributes]:
+        return super(AccountAttributesResource, self)._create({})
---
+++ method/resources/Accounts/Products.py
@@ -0,0 +1,44 @@
+from typing import TypedDict, Optional, Literal
+
+from method.resource import MethodResponse, Resource
+from method.configuration import Configuration
+from method.errors import ResourceError
+
+
+AccountProductTypeStatusLiterals = Literal[
+    'unavailable',
+    'available',
+    'restricted'
+]
+
+
+class AccountProduct(TypedDict):
+    id: str
+    name: str
+    status: AccountProductTypeStatusLiterals
+    status_error: Optional[ResourceError]
+    latest_request_id: str
+    is_subscribable: bool
+    created_at: str
+    updated_at: str
+
+
+class AccountProductListResponse(TypedDict):
+    attribute: Optional[AccountProduct]
+    balance: Optional[AccountProduct]
+    payment: Optional[AccountProduct]
+    sensitive: Optional[AccountProduct]
+    update: Optional[AccountProduct]
+    transactions: Optional[AccountProduct]
+    payoff: Optional[AccountProduct]
+    card_brand: Optional[AccountProduct]
+
+class AccountProductResource(Resource):
+    def __init__(self, config: Configuration):
+        super(AccountProductResource, self).__init__(config.add_path('products'))
+
+    def retrieve(self, prd_id: str) -> MethodResponse[AccountProduct]:
+        return super(AccountProductResource, self)._get_with_id(prd_id)
+
+    def list(self) -> MethodResponse[AccountProductListResponse]:
+        return super(AccountProductResource, self)._list()
