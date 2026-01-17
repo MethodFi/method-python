717df8dd66d703a4b2734ab38c82cb7d0eaae449
Michael Ossig <mrossig201@gmail.com>
2026-01-16 19:02:01 -0500

    bumps version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='method-python',
-    version='2.0.1',
+    version='2.1.1',
     description='Python library for the Method API',
     long_description='Python library for the Method API',
     long_description_content_type='text/x-rst',
